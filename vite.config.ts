94a8a89721c2c8e6134d1920118197f8d51b9673
WILSON JAVIER MOLINA LOZANO <molina9808.com>
2026-04-09 13:59:08 -0500

    configuracion

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+    base: '/portafolio/',
+
     plugins: [react()],
+
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
     },
 })
+
